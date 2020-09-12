3d160f67b887b8f7e6001d600ef890863b8a3f78
Qian Dong <qdong327@gmail.com>
2020-09-12 12:00:50 -0400

    added keys that are commented out, started html file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # weatherdashboard
+
+A simple weather dashboard application that pulls from server-side API's, using Javascript, jQuery and AJAX.
---
+++ docs/script.js
@@ -0,0 +1,4 @@
+//API Key: https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=c53f64d488e1a3a434d5df4073ee05e5
+
+// API Call: api.openweathermap.org/data/2.5/weather?q={city name}&appid={c53f64d488e1a3a434d5df4073ee05e5}
+
