b6a310b30e0ecea12ff0b30fc6e675018eab073d
Qian Dong <qdong327@gmail.com>
2020-09-13 19:18:46 -0400

    setup html and css framework and all divs

--- docs/script.js
+++ docs/script.js
@@ -2,3 +2,10 @@
 
 // API Call: api.openweathermap.org/data/2.5/weather?q={city name}&appid={c53f64d488e1a3a434d5df4073ee05e5}
 
+// Global Variables
+var getToday = document.querySelector("#today");
+var momentToday = moment().format("[Today is] dddd, MMMM Do");
+
+//Display Today
+// // Current Day and Time with Moment.js
+getToday.innerHTML = momentToday;
