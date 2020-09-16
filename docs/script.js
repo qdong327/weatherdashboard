acefd79ccf682b3f2520741f1096534453e0f2c1
Qian Dong <qdong327@gmail.com>
2020-09-15 23:53:26 -0400

    got cards working, missing first city icon

--- docs/script.js
+++ docs/script.js
@@ -12,6 +12,13 @@ var wind = document.querySelector("#wind-speed");
 var uvIndex = document.querySelector("#uv-index");
 var currentCity = document.querySelector("#selected-city");
 var currentCountry = document.querySelector("#selected-state");
+var currentWeatherIconUrl = "";
+var currentWeatherIconCode = "";
+var iconUrl = "";
+var iconCode = "";
+var forecastDate = "";
+var forecastTemp = "";
+var forecastHum = "";
 
 // API Info and Getting From Local Storage
 var APIKey = "c53f64d488e1a3a434d5df4073ee05e5";
@@ -27,49 +34,99 @@ if (getCitiesFromLocal !== null) {
 
 //Function to Generate Weather Information
 function generateWeatherInfo() {
-    // var response = $(this);
-    cityName = cityName.val().trim();
+    var cityNameValue = cityName.val().trim();
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" +
-        cityName + "&appid=" + APIKey;
-    console.log(queryURL);
+        cityNameValue + "&appid=" + APIKey;
     //  AJAX call to the OpenWeatherAPI
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
+        console.log(queryURL);
         currentCity.innerHTML = response.name;
         currentCountry.innerHTML = response.sys.country;
+        var currentWeatherIconEl = $("<img>").attr("src", currentWeatherIconUrl);
+        //Stupid icon doesn't work yet
+        // $('.card-body').appendChild(currentWeatherIconEl);
+        currentWeatherIconCode = response.weather[0].icon;
+        currentWeatherIconUrl = "https://openweathermap.org/img/w/" + currentWeatherIconCode + ".png";
         tempF.innerHTML = "Temperature: " + Math.round((((response.main.temp) - 273.15) * 9 / 5) + 32) + "°F / " + Math.round((response.main.temp) - 273.15) + "°C";
         hum.innerHTML = "Humidity: " + response.main.humidity + "%";
         wind.innerHTML = "Wind Speed: " + response.wind.speed + " MPH";
         var latitude = response.coord.lat;
         var longitude = response.coord.lon;
         var uvIndexQueryUrl = "https://api.openweathermap.org/data/2.5/uvi?&appid=" + APIKey + "&lat=" + latitude + "&lon=" + longitude;
-        console.log(uvIndexQueryUrl);
         $.ajax({
             url: uvIndexQueryUrl,
             method: "GET"
         })
             .then(function (response) {
                 uvIndex.innerHTML = "UV Index: " + response.value;
-                if (response.value < 10) {
-                    $("p #uv-index").addClass("green");
-                    console.log(uvIndex);
+                if (response.value < 3) {
+                    $("#uv-color").addClass("green");
+                } else if (response.value > 2 && response.value < 6) {
+                    $("#uv-color").addClass("yellow");
+                } else if (response.value > 5 && response.value < 8) {
+                    $("#uv-color").addClass("orange");
+                } else if (response.value > 7 && response.value < 11) {
+                    $("#uv-color").addClass("red");
+                } else {
+                    $("#uv-color").addClass("purple");
                 }
-                // } else if (response.value)
+            });
+    })
+}
+
+//Functions to Generate Five Day Forecast as cards
+function displayFiveDayForecast() {
+    var fiveDayDiv = $("#forecast-card-div");
+    var cardEl = $("<div class='card'>").addClass("forecast-card col-2 text-light");
+    var iconEl = $("<img>").attr("src", iconUrl);
+    var cardTitleDate = $("<h5>").text(forecastDate);
+    var cardTextDiv = $("<div>");
+    var cardTempElHigh = $("<p>").text(forecastTempH);
+    var cardTempElLow = $("<p>").text(forecastTempL);
+    var cardHumEl = $("<p>").text(forecastHum);
+    cardTextDiv.append(cardTitleDate);
+    cardTextDiv.append(cardTempElHigh);
+    cardTextDiv.append(cardTempElLow);
+    cardTextDiv.append(cardHumEl);
+    cardTextDiv.append(iconEl);
+    fiveDayDiv.append(cardEl);
+    cardEl.append(cardTextDiv);
+}
+
+function generateFiveDayForecast() {
+    var cityNameNew = cityName.val().trim();
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" +
+        cityNameNew + "&appid=" + APIKey;
+    //  AJAX call to the OpenWeatherAPI Forecast Info
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        var latitude = response.coord.lat;
+        var longitude = response.coord.lon;
+        var fiveDayQueryUrl = "https://api.openweathermap.org/data/2.5/onecall?&appid=" + APIKey + "&lat=" + latitude + "&lon=" + longitude;
+        $.when($.ajax({
+            url: fiveDayQueryUrl,
+            method: "GET"
+        }))
+            .then(function (response) {
+                for (var i = 0; i < 5; i++) {
+                    console.log(response.daily[i]);
+                    iconCode = response.daily[i].weather[0].icon;
+                    iconUrl = "https://openweathermap.org/img/w/" + iconCode + ".png";
+                    forecastDate = moment().add(i, "days").format("MMMM DD YYYY");
+                    forecastTempH = "H: " + Math.round((((response.daily[i].temp.max) - 273.15) * 9 / 5) + 32) + "°F / " + Math.round((response.daily[i].temp.max) - 273.15) + "°C";
+                    forecastTempL = "L: " + Math.round((((response.daily[i].temp.min) - 273.15) * 9 / 5) + 32) + "°F / " + Math.round((response.daily[i].temp.min) - 273.15) + "°C";
+                    forecastHum = "Humidity: " + response.daily[i].humidity + "%"
+                    displayFiveDayForecast();
+                }
             });
     }
     )
 }
-
-//Function to Add UV Index Color
-// function generateUVColor() {
-// }
-
-//Function to Generate Five Day Forecast as cards
-function generateFiveDayForecast() {
-
-};
 //Function to save city to local storage and as button
 function saveCityAndButtonToLocal() {
 
